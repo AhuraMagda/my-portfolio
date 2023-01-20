6dae973534b542b59789925c8777c06465e59ec1
AhuraMagda <lubaszka.magda@gmail.com>
2023-01-20 08:12:46 +0100

    repairing arrows

--- app.js
+++ app.js
@@ -3,23 +3,7 @@ const introText = document.getElementsByClassName('intro-text')[0];
 const logo = document.getElementById('logo')
 const arrow = document.querySelector('figure')
 
-const slide1 = document.getElementById('slide1')
-const slide2 = document.getElementById('slide2');
-const slide3 = document.getElementById('slide3');
-
-const leftDot = document.getElementById('dot-left');
-const rightDot = document.getElementById('dot-right');
-
-const seeDescriptionButton = document.getElementById('see-description');
-const hideDescriptionButton = document.getElementById('hide-description')
-
-const slideDes1 = document.getElementById('slide-des1');
-const slideDes2 = document.getElementById('slide-des2');
-const slideDes3 = document.getElementById('slide-des3');
-
-let activeSlideNumber = 2;
-
-
+// INTRO
 // znika duża grafika i przesuwa się na bok
 const introChange = () => {
     introText.style.display = 'block'
@@ -41,70 +25,30 @@ setTimeout(textVisibility, 5100);
 setTimeout(arrowShow, 6100)
 
 
-// CLICK SLIDE
+// PROJECTS
+const slide1 = document.getElementById('slide1')
+const slide2 = document.getElementById('slide2');
+const slide3 = document.getElementById('slide3');
+
+const leftDot = document.getElementById('dot-left');
+const rightDot = document.getElementById('dot-right');
+
+const seeDescriptionButton = document.getElementById('see-description');
+const hideDescriptionButton = document.getElementById('hide-description')
+
+const slideDes1 = document.getElementById('slide-des1');
+const slideDes2 = document.getElementById('slide-des2');
+const slideDes3 = document.getElementById('slide-des3');
+
+let activeSlideNumber = 2;
+
+
 let hideActiveSlide = () => {
     let activeElement = document.querySelector('.active');
     activeElement.classList.remove('active');
 }
 
-let showSlide = slideNumber => {
-    slideDes1.style.display = 'none';
-    slideDes2.style.display = 'none';
-    slideDes3.style.display = 'none';
-    hideActiveSlide();
-    document.querySelector('#slide'+slideNumber).classList.add('active')
-    if(slideNumber === 1) {
-        slide3.style.order = 1
-        slide2.style.order = 3
-    } else if(slideNumber === 2) {
-        slide1.style.order = 1
-        slide3.style.order = 3  
-    } else if(slideNumber === 3) {
-        slide2.style.order = 1
-        slide1.style.order = 3  
-    } 
-    hideDescription();
-    seeDescriptionButton.style.display = 'block';
-    hideDescriptionButton.style.display = 'none';
-}
-
-let showSlide1 = () => {
-    showSlide(1);
-    activeSlideNumber = 1;
-}
-
-let showSlide2 = () => {
-    showSlide(2);
-    activeSlideNumber = 2;
-}
-let showSlide3 = () => {
-    showSlide(3);
-    activeSlideNumber = 3;
-}
-slide1.addEventListener('click', showSlide1);
-slide2.addEventListener('click', showSlide2);
-slide3.addEventListener('click', showSlide3);
-
-
-// CAROUSEL
-let changeSlideRight = () => {
-    if(activeSlideNumber === 3) {
-        activeSlideNumber = 1;
-    } else {
-        activeSlideNumber++;
-    }
-    showSlide(activeSlideNumber)
-}
-
-let changeSlideLeft = () => {
-    if(activeSlideNumber === 1) {
-        activeSlideNumber = 3;
-    } else {
-        activeSlideNumber--;
-    }
-    showSlide(activeSlideNumber)
-}
-
+// description
 let showDescription = () => {
     if(activeSlideNumber === 1) {
         slide1.style.display = 'none';
@@ -138,9 +82,74 @@ let hideDescription = () => {
     hideDescriptionButton.style.display = 'none';
 }
 
-rightDot.addEventListener('click', changeSlideRight)
-leftDot.addEventListener('click', changeSlideLeft)
 
 seeDescriptionButton.addEventListener('click', showDescription)
 hideDescriptionButton.addEventListener('click', hideDescription)
+
+// show slide
+let showSlide = slideNumber => {
+    slideDes1.style.display = 'none';
+    slideDes2.style.display = 'none';
+    slideDes3.style.display = 'none';
+    hideActiveSlide();
+    document.querySelector('#slide'+slideNumber).classList.add('active')
+    if(slideNumber === 1) {
+        slide3.style.order = 1
+        slide2.style.order = 3
+    } else if(slideNumber === 2) {
+        slide1.style.order = 1
+        slide3.style.order = 3  
+    } else if(slideNumber === 3) {
+        slide2.style.order = 1
+        slide1.style.order = 3  
+    } 
+    hideDescription();
+    seeDescriptionButton.style.display = 'block';
+    hideDescriptionButton.style.display = 'none';
+}
+
+// click
+let showSlide1 = () => {
+    showSlide(1);
+    activeSlideNumber = 1;
+}
+
+let showSlide2 = () => {
+    showSlide(2);
+    activeSlideNumber = 2;
+}
+let showSlide3 = () => {
+    showSlide(3);
+    activeSlideNumber = 3;
+}
+
+slide1.addEventListener('click', showSlide1);
+slide2.addEventListener('click', showSlide2);
+slide3.addEventListener('click', showSlide3);
+
+
+// arrows
+let changeSlideRight = () => {
+    if(activeSlideNumber===1) {
+        showSlide2();
+    } else if(activeSlideNumber===2){
+        showSlide3();
+    } else if(activeSlideNumber===3){
+        showSlide1()
+    }
+}
+
+let changeSlideLeft = () => {
+    if(activeSlideNumber===1) {
+        showSlide3();
+    } else if(activeSlideNumber===2){
+        showSlide1();
+    } else if(activeSlideNumber===3){
+        showSlide2()
+    }
+}
+
+rightDot.addEventListener('click', changeSlideRight)
+leftDot.addEventListener('click', changeSlideLeft)
+
 
